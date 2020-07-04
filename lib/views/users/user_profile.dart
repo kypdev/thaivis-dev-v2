6b190a1b3410236bbd3ff7ddc2f748ceffc5d4eb
kypdev <kyptdev@gmail.com>
2020-07-04 23:55:53 +0700

    user profile screen

--- lib/views/users/user_profile.dart
+++ lib/views/users/user_profile.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:thaivis_dev_v2/common/cus_btn.dart';
@@ -15,7 +16,7 @@ class UserProfile extends StatefulWidget {
 
 class _UserProfileState extends State<UserProfile> {
   Auth auth = new Auth();
-  String userID = '';
+  String userID;
 
   void debugButton() {
     print('userid: $userID');
@@ -43,7 +44,6 @@ class _UserProfileState extends State<UserProfile> {
 
   @override
   void dispose() {
-    
     super.dispose();
   }
 
@@ -60,36 +60,125 @@ class _UserProfileState extends State<UserProfile> {
             ),
           ),
           centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(
+                  Icons.person,
+                ),
+                onPressed: debugButton),
+          ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              
-              cusBtn(
-                action: ()=> debugButton(),
-                color: Colors.green,
-                text: 'debug',
+        body: StreamBuilder(
+          stream: Firestore.instance
+              .collection('users')
+              .document(userID)
+              .snapshots(),
+          builder: (context, sn) {
+            if (!sn.hasData) {
+              return Text('Loading data Please wait...');
+            }
+
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundColor: Colors.blue,
+                      radius: 75.0,
+                      child: CircleAvatar(
+                        radius: 70.0,
+                        backgroundColor: Color(0XFFFFFFFF),
+                        backgroundImage: NetworkImage(sn.data['imgpro']),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    // Text(sn.data['firstname']),
+                    // Text(sn.data['lastname']),
+                    // Text(sn.data['email']),
+
+                    _form(
+                      title: 'ชื่อ',
+                      content: sn.data['firstname']
+                    ),
+
+                    _form(
+                      title: 'นามสกุล',
+                      content: sn.data['lastname']
+                    ),
+
+                    _form(
+                      title: 'อีเมล',
+                      content: sn.data['email']
+                    ),
+
+
+
+
+
+
+                    SizedBox(height: 20.0),
+                    cusBtn(
+                      action: () => logout(),
+                      color: Colors.red,
+                      text: 'ออกจากระบบ',
+                    ),
+                  ],
+                ),
               ),
-
-              SizedBox(height: 20.0),
-
-
-
-
-              cusBtn(
-                action: () => logout(),
-                color: Colors.red,
-                text: 'ออกจากระบบ',
-              ),
-
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
   }
-
-  
 }
+
+// cusBtn(
+//                 action: ()=> debugButton(),
+//                 color: Colors.green,
+//                 text: 'debug',
+//               ),
+
+//               SizedBox(height: 20.0),
+
+//               cusBtn(
+//                 action: () => logout(),
+//                 color: Colors.red,
+//                 text: 'ออกจากระบบ',
+//               ),
+
+
+ Widget _form({
+    title,
+    content,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 22.0,
+            ),
+          ),
+          Text(
+            content,
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.black54,
+            ),
+          ),
+          Divider(
+            thickness: 2,
+            color: Colors.black45,
+          ),
+        ],
+      ),
+    );
+  }
